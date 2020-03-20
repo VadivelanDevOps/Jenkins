e127daf2549ceacb68605b116d8a631fcbeeb31e
VadivelanDevOps <62283794+VadivelanDevOps@users.noreply.github.com>
2020-03-20 12:46:48 +0530

    Update firstprograminjava.java

--- firstprograminjava.java
+++ firstprograminjava.java
@@ -5,5 +5,9 @@ class firstprograminjava
 	public static void main (String args[])
 		{
 			System.out.println("This is my first program in java");
+			for (int i=1; i<=10; i++)
+			{
+				System.out.println(i);
+			}
 		}
 }
