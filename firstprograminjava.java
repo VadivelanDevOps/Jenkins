acf27e3939cbadff356c1ac69715bec28fad45e7
VadivelanDevOps <62283794+VadivelanDevOps@users.noreply.github.com>
2020-03-20 07:54:37 +0530

    Add files via upload

---
+++ firstprograminjava.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class firstprograminjava
+{
+	public static void main (String args[])
+		{
+			System.out.println("This is my first program in java");
+		}
+}
